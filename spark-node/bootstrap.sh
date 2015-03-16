fcd01ee9cac4becab3e21dc434a62901633d346a
netknight <netknight@hot.ee>
2015-03-16 13:05:35 +0200

    Added spark configuration

---
+++ spark-node/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HADOOP_HOME=/opt/hadoop
+
+#$HADOOP_HOME/etc/hadoop/hadoop-env.sh
+
+# installing libraries if any - (resource urls added comma separated to the ACP system variable)
+#cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+
+# altering the core-site configuration
+#sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+
+service sshd start
+
+#$HADOOP_PREFIX/sbin/start-dfs.sh
+#$HADOOP_PREFIX/sbin/start-yarn.sh
+
